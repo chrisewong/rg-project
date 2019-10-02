f90e70249998d68ffe901c59f5f87ed3e662ac23
Chris <chrisewong@gmail.com>
2019-10-01 21:35:11 -0700

    first commit

--- header.php
+++ header.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
   <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
   </head>
 
   <body <?php body_class(); ?>>
+   <div class="background">
     <header class="navbar">
       <div class="container">
         <div class="logo">
-        <h1 class="float-left"><a href="<?php echo site_url() ?>">RG Installations</a></h1>
+        <h1 class="float-left"><a href="<?php echo site_url() ?>">RG WINDOW COVERINGS</a></h1>
         </div>
         
         <ul class="menu">
--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php get_header(); ?>
 
-
-<!-- <div class="box">
-  <div class="background">
-    <div class="img" alt="image">
-  <img />
+<div class="top">
+  <div class="intro">
+  <h1>RG Window Coverings</h1>
+  <br />
+  <h2>Family Owned and Operated for 10 years</h2>
   </div>
-</div> -->
+</div>
 
 
 
-
-
-  <div id="slider">
+<div id="slider">
 <figure>
 <img src="http://rg.local/wp-content/uploads/2019/09/shutter1-e1569273253424.jpg" alt>
 <img src="http://rg.local/wp-content/uploads/2019/09/shutter1-e1569273253424.jpg" alt>
-<img src="http://rg.local/wp-content/uploads/2019/09/shutter1-e1569273253424.jpg" alt>
+<img src="http://rg.local/wp-content/uploads/2019/09/shutter2.jpg" alt>
 </figure>
 </div>
+
 
   <!-- <div class="button-container">
     
@@ -27,20 +26,54 @@
   <a href="#slider-image-3" class="slider"></a>
 
 </div> -->
+<div class="info">
+  <div class="description">
+    <p>
+    <h2>
+    &#10004; Shutter Installation
+</h2>
+    <h2>
+    &#10004; Roller Shades
+      </h2>
+      <h2>
+    &#10004; Blinds
+      </h2>
+</p>
+      </div>
 
-
-
-  <div class="info">
-    <div class="page">
-<h1>
-  Shutter Installation
-  <hr>
-  Carpet Installation
-  </h1>
+      <div class="float-right">
+      <h2>
+        blog
+      </h2>
       </div>
     </div>
-  </div>
-  </div>
+
+    
+
+
+<div class="comments">
+  <form>
+  <label for="fname">FIRST NAME</label>
+  <br>
+  <input type="text" id="fname" name="fname">
+  <br>
+  <label for="lname">LAST NAME</label>
+  <br>
+  <input type="text" id="lname" name="lname">
+  <br>
+  <label for=”userName”>EMAIL</label>
+  <br>
+	<input type=”email” id=”email” name=”email” /><br/>
+  <br>
+  <label for=”phone”>TELEPHONE</label>
+  <br>
+	<input type=”tel” id=”phone” name=”phone” />
+
+</form>
+</div>
+
+
+
 
 <?php
   get_footer();
